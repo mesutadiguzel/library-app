30503029bfc46ac6cf9302c3f59a16a9fc2a3471
Mesut Adiguzel <mesutadiguzel@Mesuts-MacBook-Air.local>
2025-11-26 10:53:02 +0000

    Api Gateway created

---
+++ api-gateway/src/main/java/com/mesut/adiguzel/apps/libraryapp/ApiGatewayApp.java
@@ -0,0 +1,15 @@
+package com.mesut.adiguzel.apps.libraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ApiGatewayApp.class, args);
+    }
+}
